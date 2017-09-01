97fed83d1380b0da386bc18a9214b7f35d17c55e
Eugen Mayer <eugen.mayer@kontextwork.de>
2017-09-01 10:53:06 +0200

    fix wrong static value

--- resize.sh
+++ resize.sh
@@ -19,7 +19,7 @@ APPLY=$3
 # So get the disk-informations of our device in question printf %s\\n 'unit MB print list' | parted | grep "Disk /dev/sda we use printf %s\\n 'unit MB print list' to ensure the units are displayed as MB, since otherwise it will vary by disk size ( MB, G, T ) and there is no better way to do this with parted 3 or 4 yet
 # then use the 3rd column of the output (disk size) cut -d' ' -f3 (divided by space)
 # and finally cut off the unit 'MB' with blanc using tr -d MB
-MAXSIZEMB=`printf %s\\n 'unit MB print list' | parted | grep "Disk /dev/sda" | cut -d' ' -f3 | tr -d MB`
+MAXSIZEMB=`printf %s\\n 'unit MB print list' | parted | grep "Disk #{DEVICE}" | cut -d' ' -f3 | tr -d MB`
 
 echo "will resize to ${MAXSIZEMB}MB"
 
