f0a0bae412de42e2133f771630c465a1e0c8a0fe
Eugen Mayer <eugen.mayer@kontextwork.de>
2018-07-18 13:42:06 +0200

    check for the existence of the drive

--- resize.sh
+++ resize.sh
@@ -16,6 +16,8 @@ DEVICE=$1
 PARTNR=$2
 APPLY=$3
 
+fdisk -l $DEVICE$PARTNR &>1 /dev/null || (echo "could not find device $DEVICE$PARTNR - please check the name" && exit 1)
+
 CURRENTSIZEB=`fdisk -l $DEVICE$PARTNR | grep "Disk $DEVICE$PARTNR" | cut -d' ' -f5`
 CURRENTSIZE=`expr $CURRENTSIZEB / 1024 / 1024`
 # So get the disk-informations of our device in question printf %s\\n 'unit MB print list' | parted | grep "Disk /dev/sda we use printf %s\\n 'unit MB print list' to ensure the units are displayed as MB, since otherwise it will vary by disk size ( MB, G, T ) and there is no better way to do this with parted 3 or 4 yet
